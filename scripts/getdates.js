1e413659bbe74dd294694223623f1b8551fb04ae
kkmorrisfam <kkmorrisfam@hotmail.com>
2024-01-12 19:07:46 -0800

    Fixed name and picture

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,18 @@
 //Date().getFullYear()
-let date =  new Date().getFullYear();
-console.log(date);
+
+const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+};
+
+let todaysDate =  new Date();
+let lastModified = new Date(document.lastModified);
+
+document.querySelector("#year").textContent = todaysDate.getFullYear();
+document.querySelector('#lastModified').textContent = lastModified.toLocaleDateString("en-UK", options);
+
+
+//this is for current date/time. I need document last modified date.
+// document.querySelector('#lastModified').textContent = date.toLocaleDateString("en-UK", options);
