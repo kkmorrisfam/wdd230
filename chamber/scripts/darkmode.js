384c09efd8776d957ba4536f27f51a0228f3a3b4
kkmorrisfam <kkmorrisfam@hotmail.com>
2024-02-09 18:11:04 -0800

    update toggle image

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -6,7 +6,7 @@ const darkModeToggle = document.querySelector('#dark-toggle');
 //when the button is clicked
 darkModeToggle.addEventListener('click', () => {	
     //toggle on the dark-mode class
-	// darkModeToggle.classList.toggle('dark-mode');
+	darkModeToggle.classList.toggle('dark-mode');
     document.body.classList.toggle('dark-mode');
     console.log()
 });
