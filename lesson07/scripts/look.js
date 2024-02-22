c5873aa8b5bee193c11dac1a4c7bea72bc9da31a
kkmorrisfam <kkmorrisfam@hotmail.com>
2024-02-21 21:03:45 -0800

    update javascript bom

--- lesson07/scripts/look.js
+++ lesson07/scripts/look.js
@@ -2,31 +2,42 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
+//1. return local storage item, or if empty, assign variable with empty array
 let chaptersArray = getChapterList() || [];
+console.log(chaptersArray);
 
-// Function to set the chapter list in localStorage
-function setChapterList() {
-    localStorage.setItem('chapters', JSON.stringify(chaptersArray));
-}
+//2. populate the list array
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+    console.log("inside chaptersArray.forEach")
+    console.log(chaptersArray);
+});
 
-// Function to get the chapter list from localStorage
-function getChapterList() {
-    // const storedChapters = localStorage.getItem('chapters');
-    // return storedChapters ? JSON.parse(storedChapters) : [];
+// 3. element.addEventListener(event, function, useCapture);
+button.addEventListener("click", ()=> {
+    console.log("Inside eventListener")
+    console.log(input.value);
+    
+    //check if the input is empty or not
+    if (input.value != "")
+    {
+        displayList(input.value);         //add new input item to list
+        chaptersArray.push(input.value);  //add new input item to array
+        setChapterList();                 //update local storage with updated/new array
+        input.value = "";         //reset input to blank
+        input.focus();                    //return focus
+        
+    }
+    // could add hint message if true to the box.
+    //return input focus to input if nothing entered when button is clicked.
+    else {
+        console.log("inside eventListener else section.")
+        input.focus();
+    }    
+})
 
-    return JSON.parse(localStorage.getItem('chapters'));
-}
-  
-//reformat the chapter parameter to get rid of the X that is passed on hte end of hte chapter string
-function deleteChapter(chapter) {
-    chapter = chapter.slice(0, chapter.length - 1);
-    //return everything in the chapterArray except the chapter to be deleted.
-    chaptersArray = chaptersArray.filter((item)=> item !==chapter);
-    //update localstorage
-    setChapterList();    
-}
 
-//function to display the list
+// 4. function to display the list
 // redo eventListener as function
 function displayList(item) {
     console.log("inside displayList");
@@ -34,49 +45,41 @@ function displayList(item) {
     let deleteButton = document.createElement("button");        
     li.textContent = item;  //changed from input.value
     deleteButton.textContent = "✖";
-    deletebutton.classList.add('delete');  //added a new class
+    deleteButton.classList.add('delete');  //added a new class
     li.append(deleteButton);
     list.append(li);
     
     deleteButton.addEventListener("click", ()=>{
-    list.removeChild(li);
-    deleteChapter(li.textContent);  //added
-    input.focus();
-    })
-    
+        list.removeChild(li);
+        deleteChapter(li.textContent);  //added
+        input.focus();
+    })    
     //do I need this?
     // input.focus();
     // input.value = "";
 }
 
 
-//populate the list array
-chaptersArray.forEach(chapter => {
-    displayList(chapter);
-    console.log("inside chaptersArray")
-    console.log(chaptersArray);
-});
+// 5. Function to set the chapter list in localStorage
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
 
-
-// element.addEventListener(event, function, useCapture);
-
-button.addEventListener("click", ()=> {
-    //check if the input is empty or not
-    if (input.value != "")
-    {
-        console.log(input.value);
-        displayList(input.value);         //add new input item to list
-        chaptersArray.push(input.value);  //add new input item to array
-        setChapterList();                 //update local storage with updated/new array
-        input.focus();                    //return focus
-        input.value = "";        //reset input to blank
-    }
-    // could add hint message if true to the box.
-    //return input focus to input if nothing entered when button is clicked.
-    else {
-        input.focus();
-    }
-   
-    
-})
-
+// 6. Function to get the chapter list from localStorage
+function getChapterList() {
+    // const storedChapters = localStorage.getItem('chapters');
+    // return storedChapters ? JSON.parse(storedChapters) : [];
+    console.log("inside getChaptersList()")
+    console.log(localStorage.getItem('myFavBOMList'))
+    // console.log(chaptersArray);
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+  
+// 7.reformat the chapter parameter to get rid of the X that is passed on hte end of hte chapter string
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    //return everything in the chapterArray except the chapter to be deleted.
+    chaptersArray = chaptersArray.filter((item)=> item !==chapter);
+    //update localstorage
+    setChapterList();    
+}
