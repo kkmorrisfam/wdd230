af38a8c18015686a05a10e4b472d291ebe02e10f
kkmorrisfam <kkmorrisfam@hotmail.com>
2024-02-21 21:16:54 -0800

    comment out console.log

--- lesson07/scripts/look.js
+++ lesson07/scripts/look.js
@@ -4,19 +4,19 @@ const list = document.querySelector("#list");
 
 //1. return local storage item, or if empty, assign variable with empty array
 let chaptersArray = getChapterList() || [];
-console.log(chaptersArray);
+// console.log(chaptersArray);
 
 //2. populate the list array
 chaptersArray.forEach(chapter => {
     displayList(chapter);
-    console.log("inside chaptersArray.forEach")
-    console.log(chaptersArray);
+    // console.log("inside chaptersArray.forEach")
+    // console.log(chaptersArray);
 });
 
 // 3. element.addEventListener(event, function, useCapture);
 button.addEventListener("click", ()=> {
-    console.log("Inside eventListener")
-    console.log(input.value);
+    // console.log("Inside eventListener")
+    // console.log(input.value);
     
     //check if the input is empty or not
     if (input.value != "")
@@ -31,7 +31,7 @@ button.addEventListener("click", ()=> {
     // could add hint message if true to the box.
     //return input focus to input if nothing entered when button is clicked.
     else {
-        console.log("inside eventListener else section.")
+        // console.log("inside eventListener else section.")
         input.focus();
     }    
 })
@@ -40,7 +40,7 @@ button.addEventListener("click", ()=> {
 // 4. function to display the list
 // redo eventListener as function
 function displayList(item) {
-    console.log("inside displayList");
+    // console.log("inside displayList");
     let li = document.createElement("li");
     let deleteButton = document.createElement("button");        
     li.textContent = item;  //changed from input.value
@@ -62,17 +62,17 @@ function displayList(item) {
 
 // 5. Function to set the chapter list in localStorage
 function setChapterList() {
-    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+    localStorage.setItem('chaptersKey', JSON.stringify(chaptersArray));
 }
 
 // 6. Function to get the chapter list from localStorage
 function getChapterList() {
     // const storedChapters = localStorage.getItem('chapters');
     // return storedChapters ? JSON.parse(storedChapters) : [];
-    console.log("inside getChaptersList()")
-    console.log(localStorage.getItem('myFavBOMList'))
-    // console.log(chaptersArray);
-    return JSON.parse(localStorage.getItem('myFavBOMList'));
+
+    // console.log("inside getChaptersList()")
+    // console.log(localStorage.getItem('chaptersKey'))    
+    return JSON.parse(localStorage.getItem('chaptersKey'));
 }
   
 // 7.reformat the chapter parameter to get rid of the X that is passed on hte end of hte chapter string
