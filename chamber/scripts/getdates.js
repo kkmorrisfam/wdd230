728cacbb7623b7e3bb5c9ee99d09c3f4575b4b66
kkmorrisfam <kkmorrisfam@hotmail.com>
2024-02-22 20:11:44 -0800

    updates to discover and home

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -15,6 +15,7 @@ let dateNow = Date.now();
 let todaysDate =  new Date();
 let lastModified = new Date(document.lastModified);
 
+
 console.log(dateNow);
 document.querySelector("#year").textContent = todaysDate.getFullYear();
 document.querySelector('#lastModified').textContent = `Last Modification: ${lastModified.toLocaleDateString("en-UK", options)}`;
@@ -25,7 +26,25 @@ document.querySelector('#lastModified').textContent = `Last Modification: ${last
 
 // 1. return local storage item, or if empty, assign variable with empty array
 let dateVisited = Number(window.localStorage.getItem("dateVisit-Key")) || 0;
-console.log(dateVisited);
+// console.log(dateVisited);
+// console.log(`dateVisited ${dateVisited}`);
+//test numbers:
+// yesterday
+// dateVisited = (dateNow - msToDays);
+// this morning
+// dateVisited = (dateNow - (msToDays * 1.5));
+// last week
+// dateVisited = (dateNow - (msToDays * 7));
+
+// console.log(`dateVisited ${dateVisited}`);
+// let days = Math.floor((dateNow-dateVisited)/msToDays);
+// console.log(`let days = (dateNow-dateVisited)/msToDays: ${days}`);
+
+// days = days / msToDays;
+// console.log(days);
+
+
+
 
 // 2. check date variable against today's date/time.  if same, display message, 
 // else if difference is less than 24hrs, display message, else display another message
@@ -36,7 +55,7 @@ if (dateVisited == 0) {
 else if ((dateNow - dateVisited) < msToDays) {
     document.querySelector("#message").textContent = "Back so soon! Awesome!";
 } else  {
-    let days = (dateNow-dateVisited)/msToDays;
+    let days = Math.floor((dateNow-dateVisited)/msToDays);    
     let word = "days";
     if (days == 1) {
         word = "day";
