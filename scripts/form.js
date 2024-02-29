e8aacf0cd3a70336ad61da1128c815807b845d16
kkmorrisfam <kkmorrisfam@hotmail.com>
2024-02-28 21:02:06 -0800

    form additions

---
+++ scripts/form.js
@@ -0,0 +1,34 @@
+const rangevalue = document.getElementById("rangeDiv");
+const range = document.getElementById("range");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
+
+// password matching
+
+const password = document.querySelector("#password");
+const password2 = document.querySelector("#password2");
+const message = document.querySelector("#formMessage");
+
+password2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+	if (password.value !== password2.value) {
+		message.textContent = "❗Passwords DO NOT MATCH!";
+		message.style.visibility = "show";
+		password2.style.backgroundColor = "#fff0f3";
+		password.value = "";
+        password2.value = "";
+		password.focus();
+	} else {
+		message.style.display = "none";
+		password2.style.backgroundColor = "#fff";
+		password2.style.color = "#000";
+	}
+}
