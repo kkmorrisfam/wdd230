1dbb648ed6d35b3f87700fdd30945cf6036af3e5
kkmorrisfam <kkmorrisfam@hotmail.com>
2024-03-06 13:33:08 -0800

    update links

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,11 @@
 const baseURL = "https://kkmorrisfam.github.io/wdd230/";
 const linksURL = "https://kkmorrisfam.github.io/wdd230/scripts/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json;
+    console.log(data);
+
+} 
+
+getLinks();
