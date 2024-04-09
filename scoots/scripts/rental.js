adc5946488ac4b7d518b537c1398015389ef1906
kkmorrisfam <kkmorrisfam@hotmail.com>
2024-04-09 12:23:43 -0700

    clean up fix errors

--- scoots/scripts/rental.js
+++ scoots/scripts/rental.js
@@ -5,7 +5,7 @@ async function jsonFetch() {
         const response = await fetch(rentalJSON);
         if (response.ok) {
             const data = await response.json();
-            console.log(data);
+            
             displayTable(data);
             displayCards(data);
         } else{
@@ -19,7 +19,7 @@ async function jsonFetch() {
 function displayTable(data) {
     const tbody = document.getElementById('table-body');
     tbody.innerHTML ="";
-    console.log(data.rentals);
+    
 
     data.rentals.forEach((rental) => {
         let tr = document.createElement("tr");
@@ -30,13 +30,12 @@ function displayTable(data) {
         let walk3hr = document.createElement("td");
         let walkFull = document.createElement("td");
 
-        // console.log(rental.type);
+        
         type.textContent = rental.type;
         max.textContent = rental.capacity;
-        // console.log(rental.reservation[0].half_day);
+        
         reserve3hr.textContent = `$${rental.reservation[0].half_day}`;
-        // console.log(rental.walk_in[0].full_day);
-        // console.log(rental.walk_in[0].half_day);
+        
         reserveFull.textContent = `$${rental.reservation[0].full_day}`;
         walk3hr.textContent = `$${rental.walk_in[0].half_day}`;
         walkFull.textContent = `$${rental.walk_in[0].full_day}`;
